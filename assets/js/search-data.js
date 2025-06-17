4f218d9122a646de06801c8f6d62b0269f240e99
Jas000n <77543568+Jas000n@users.noreply.github.com>
2025-06-17 03:43:01 +0000

    Deploying to gh-pages from @ DreamXLab/DreamXLab.github.io@9420d869ac2d882aee0b110dc6465b11a314556f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,11 +45,6 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
@@ -105,6 +100,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Urban%20Air%20Mobility/";
+            },},{id: "projects-ai-copilot-in-surgery",
+          title: 'AI copilot in Surgery',
+          description: "Redefine Surgery Intelligence",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/surgical_robot_system/";
             },},{
         id: 'social-email',
         title: 'email',
